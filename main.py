a04df2e7c496664fccb6a2a2417b1588de286485
citomirella <150628385+citomirella@users.noreply.github.com>
2023-11-13 17:57:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+nome = "Mirella"
+sobrenome = "Cito Botti"
+print(nome + " " + sobrenome)
+print(len(sobrenome))
+
+frase = "Python é uma linguagem de programação incrível!"
+frase_maiuscula = frase.upper()
+print(frase_maiuscula)
+
+print(frase.replace("incrivel", "legal"))
